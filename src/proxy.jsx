7aec3c16623c81fd283626f4ae00d69d47dece7b
Eros Flugel <erosflugel18@gmail.com>
2025-11-06 18:11:41 -0400

    first commit

---
+++ src/proxy.jsx
@@ -0,0 +1,36 @@
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+import { decrypt } from './app/lib/session';
+
+const protectedRoutes = ['/dashboard'];
+const publicRoutes = ['/login'];
+
+export default async function proxy(req) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get('session')?.value;
+  const session = await decrypt(cookie);
+
+  // Redirect to login if user tries to go to a protected route by config matcher
+  if (isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
+  }
+
+  // Redirect to /dashboard if user is already logged in
+  if (isPublicRoute && session?.userId) {
+    return NextResponse.redirect(new URL('/dashboard', req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/dashboard/:path*', // Matches /dashboard and any sub-paths
+    '/profile', // Matches /profile exactly
+    // '/api/:path*', // Matches /api and any sub-paths
+  ],
+};
